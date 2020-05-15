87215bf0a3c86cd21c7fabc9915ab8d96dc90178
David Jaimes <david@djaimes.com>
2020-05-15 00:25:18 -0700

    add react icons lib

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,31 +3,28 @@ import React from 'react'
 
 // Bootstrap Dependencies
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+import {Navbar, Nav, Container, Image} from 'react-bootstrap'
+import { FaGithub } from 'react-icons/fa'
 
+import Logo from "../images/favicon.png"
 
 const NavigationPage = () => {
 
   return (
     <Navbar bg="dark" expand="lg" variant="dark">
-      <Navbar.Brand href="/">David Jaimes</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-      <Navbar.Collapse id="responsive-navbar-nav" bg="dark">
-        <Nav className="mr-auto">
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/blog">Blog</Nav.Link>
-          <NavDropdown title="Documentation" id="responsive-nav-dropdown">
-            <NavDropdown.Item href="/astronomy">Astronomy</NavDropdown.Item>
-            <NavDropdown.Item href="/analysis">Data Analysis</NavDropdown.Item>
-            <NavDropdown.Item href="/macOS">macOS</NavDropdown.Item>
-            <NavDropdown.Item href="/data">Data Files</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Disabled Text Area" className="mr-2" disabled="True" size="sm" style={{width: "250px"}}/>
-          <Button variant="outline-light" size="sm">Disabled Search</Button>
-        </Form>
-      </Navbar.Collapse>
+      <Container>
+        <Navbar.Brand href="/">
+          <Image src={Logo} style={{height: "30px", marginRight: "20px"}}/>David Jaimes
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav" bg="dark" className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/data">Data Analysis</Nav.Link>
+            <Nav.Link href="/macOS">macOS Configuration</Nav.Link>
+            <Nav.Link href="/github"><FaGithub /> GitHub</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   )}
 
